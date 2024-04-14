eaa1423f53a7de4c37ac860c33bf01ed73d19d46
andrewnaiemf <andrewnaiemf@gmail.com>
2024-04-14 13:19:34 +0200

    fix room type relation

--- app/Http/Controllers/API/Provider/RoomController.php
+++ app/Http/Controllers/API/Provider/RoomController.php
@@ -22,7 +22,7 @@ class RoomController extends Controller
         $perPage = $request->header('per_page', 10);
         $provider = Provider::where('user_id',auth()->user()->id)->first();
 
-        $rooms = Room::with(['room_type','beds'])
+        $rooms = Room::with(['roomType','beds'])
                 ->where('provider_id', $provider->id)
                 ->simplePaginate($perPage);
 
@@ -116,7 +116,7 @@ class RoomController extends Controller
      */
     public function show($id)
     {
-        $room = Room::with(['room_type','beds'])->findOrFail($id);
+        $room = Room::with(['roomType','beds'])->findOrFail($id);
 
         return $this->returnData($room);
     }
