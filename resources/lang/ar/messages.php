d14bbfd7d6c75f38b357342a22a5f2a5f020d689
andrewnaiemf <andrewnaiemf@gmail.com>
2024-04-14 14:31:48 +0200

    fix trans

--- resources/lang/ar/messages.php
+++ resources/lang/ar/messages.php
@@ -3,7 +3,9 @@
 return [
     'rating_message' => ':sender_name قدم لك :stars نجمة ',
     'New_booking' => ':sender_name  قام بحجز جديد ',
-    'messages.provider_Completed_booking' => 'تم الحجز بنجاح',
+    'messages.provider_Completed_booking' => 'تم انتهاء الحجز بنجاح',
+    'provider_Completed_booking' => 'تم الحجز بنجاح',
+    'messages.provider_Rejected_booking' => ' رفض حجزك',
     'provider_Rejected_booking' => ' رفض حجزك',
     'new_order_messages' => ':sender_name  قام بطلب اوردر جديد ',
     'your_order_pending' => 'طلبك قيد الانتظار الآن',
@@ -12,6 +14,8 @@ return [
     'new_order' => 'لديك طلب جديد',
     'rating_message' => 'لديك تقييم',
     'messages.provider_Expired_booking' => 'لقد تم انتهاء حجزك',
+    'provider_Expired_booking' => 'لقد تم انتهاء حجزك',
+    'provider_Today_booking' => 'لديك حجز اليوم',
     'messages.provider_Today_booking' => 'لديك حجز اليوم',
 
 ];
